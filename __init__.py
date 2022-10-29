0443a257d09a05e0b1993b2ce6af5f213d5ec109
Albert <albert@skyciv.com>
2022-10-29 18:49:33 +0800

    Added PyPi inital installed

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 18:23:16 2022
+
+@author: albert pamonag
+"""
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 18:24:47 2022
+
+@author: albertp16
+"""
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Geotechnical Engineering Python library'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="geotechnicalpy",
+    version=VERSION,
+    author="albertp16 (Albert Pamonag)",
+    author_email="<albertpamonag@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers, Acadamics",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
